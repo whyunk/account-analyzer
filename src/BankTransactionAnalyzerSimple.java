ddaa279e669b425ce5bcd10aaab96b1fe335564d
Joseph Kim <whyunk79@gmail.com>
2023-11-06 15:20:35 +0900

    feat: calculate amount of all transactions for a specific month

--- src/BankTransactionAnalyzerSimple.java
+++ src/BankTransactionAnalyzerSimple.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
@@ -19,6 +22,19 @@ public class BankTransactionAnalyzerSimple {
         }
 
         System.out.println("The total for all transactions is " + total);
+
+        double monthTotal = 0;
+        final DateTimeFormatter DATE_PARRERN = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        for (final String line: lines) {
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PARRERN);
+            if(date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                monthTotal += amount;
+            }
+        }
+
+        System.out.println("The total for all transactions in January is " + monthTotal);
 
     }
 }
