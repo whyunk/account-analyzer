b7138b95b6048e37c7ef6630286100280e552d24
Joseph Kim <whyunk79@gmail.com>
2023-11-07 14:32:47 +0900

    refactor: findTransactions() using functional interface

--- src/main/java/BankStatementProcessor.java
+++ src/main/java/BankStatementProcessor.java
@@ -70,6 +70,18 @@ public class BankStatementProcessor {
         return maxAmount;
     }
 
+    public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
+        final List<BankTransaction> result = new ArrayList<>();
+
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            if (bankTransactionFilter.test(bankTransaction)) {
+                result.add(bankTransaction);
+            }
+        }
+
+        return result;
+    }
+
     public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
         final List<BankTransaction> result = new ArrayList<>();
 
